afd63da109aeae7d6490fecfb15e243f00b36335
Chaitanya Sase <chaitanya2sase@gmail.com>
2023-03-06 20:56:56 +0530

    footer  added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {FiInstagram} from "react-icons/fi"
+
+const Footer = () => {
+
+    return (
+    <>
+        <Footer>
+            <div className="footer_heading">
+                <div className="footer_media">
+                     <FiInstagram/>hello
+                </div>
+
+            </div>
+        </Footer>
+    </>
+)}
+export default Footer
